6e134027e041be4f59a18f8981c143c90aed530a
Timothy Wyld <timwyld@gmail.com>
2019-09-04 02:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ function happyHolidaysTo(name){
 
 }
 
-function happyHolidayTo(name,holiday){
-  return `Happy, ${holiday}, ${name}!`
+function happyHolidayTo(name, holiday){
+  return `Happy ${holiday}, ${name}!`
 
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+
+  
 }
